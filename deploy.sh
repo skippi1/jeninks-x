bbeca4421053fcfdf5b737c3ddbc438db3476af3
skippi1 <skippi1@gmx.de>
2021-03-07 16:01:03 +0100

    chapter 1

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+eval $(minikube docker-env)
+
+kubectl.exe create secret generic mysql-secret --from-literal='username=root' --from-literal='password=password' --from-literal='root-password=password'
+
+kubectl.exe apply -f kubernetes/secrets
+kubectl.exe apply -f kubernetes/deployments
+kubectl.exe apply -f kubernetes/services
